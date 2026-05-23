e2bb4f9a547c3a154cdb14d776fa9929c35594bd
Keirrison cauet <keirrisoncauet31@gmail.com>
2026-05-23 10:44:44 -0300

    dependencias e prisma pro mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
 
 app.get('/usuarios', (req, res) => {
-    res.send('ok, foi') // uma resposta sendo enviara pelo get
+    res.status(200).json(users) // uma resposta sendo enviara pelo get
 })
 
 app.listen(3000)
+
+
+/*
+    API para os usuários
+
+    - Criar um usuário
+    - Listar todos os usuários
+    - Editar um usuário
+    - Deletar um usuário
+*/
